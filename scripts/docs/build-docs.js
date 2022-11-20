6da5e404f4d25be7d6eac7995da9d57271b7e323
theajack <1506282385@qq.com>
2022-11-20 19:06:10 +0800

    feat: build docs

--- scripts/docs/build-docs.js
+++ scripts/docs/build-docs.js
@@ -3,12 +3,13 @@
  * @Date: 2022-11-20 18:49:51
  * @Description: Coding something
  * @LastEditors: chenzhongsheng
- * @LastEditTime: 2022-11-20 19:01:55
+ * @LastEditTime: 2022-11-20 19:05:26
  */
 const fs = require('fs');
 const {resolveRootPath} = require('../build/utils');
 
-fs.mkdirSync(resolveRootPath('docs'))
+if(!fs.existsSync(resolveRootPath('docs')))
+  fs.mkdirSync(resolveRootPath('docs'))
 
 // fs.copyFileSync(
 //   resolveRootPath('packages/term/dist/webos-term.min.js'), 
@@ -17,4 +18,8 @@ fs.mkdirSync(resolveRootPath('docs'))
 fs.copyFileSync(
   resolveRootPath('scripts/docs/index.html'), 
   resolveRootPath('docs/index.html')
+);
+fs.copyFileSync(
+  resolveRootPath('.gitignore'), 
+  resolveRootPath('docs/.gitignore')
 );
