e718356c1e98e4d1a4c75dd676554ab45d08d2cf
chenzhongsheng <chenzhongsheng@bytedance.com>
2023-02-04 16:58:24 +0800

    feat: export disk and module

--- packages/term/src/index.ts
+++ packages/term/src/index.ts
@@ -2,7 +2,9 @@
  * @Author: chenzhongsheng
  * @Date: 2022-11-10 09:26:51
  * @Description: Coding something
- * @LastEditors: chenzhongsheng
- * @LastEditTime: 2022-11-10 10:40:26
  */
 export * from './term';
+
+export * from 'webos-disk';
+
+export * from 'webos-module';
